dd90da30f246a2be51e6d7be26c41912e068e2b9
Daniel Plundrich <dan.plundrich@gmail.com>
2022-12-28 12:34:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     let x = document.querySelector(".nav-elements");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
