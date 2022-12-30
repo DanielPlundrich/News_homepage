dd61dfa650c6a4613dcab4a2d8fbbeb5fe9553d2
Daniel Plundrich <dan.plundrich@gmail.com>
2022-12-30 15:44:56 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// function myFunction() {
-//     let x = document.querySelector(".nav-elements");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
+function myFunction() {
+    let x = document.querySelector(".nav-elements");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+    let y = document.querySelector(".nav-bg");
+        if (y.style.display === "block") {
+          y.style.display = "none";
+        } else {
+          y.style.display = "block";
+        }
+  }
+
+
